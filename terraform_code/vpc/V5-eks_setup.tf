25f6e2f600e1c0271e9c1f2efce0ce47ed61ec50
emdadripon <eriponva@gmail.com>
2024-06-25 12:59:16 -0400

    code changes

--- terraform_code/vpc/V5-eks_setup.tf
+++ terraform_code/vpc/V5-eks_setup.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 resource "aws_instance" "demo-server" {
@@ -62,7 +62,7 @@ resource "aws_subnet" "dpp-public-subnet-01" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   tags = {
     Name = "dpp-public-subent-01"
   }
@@ -72,7 +72,7 @@ resource "aws_subnet" "dpp-public-subnet-02" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-1b"
   tags = {
     Name = "dpp-public-subent-02"
   }
